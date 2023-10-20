ee4b652e55dbe612ae6ab226ac07aa8c8efc775d
Clara Aquino <mclaraaquino1@gmail.com>
2023-10-19 22:54:21 -0300

    Criação dos métodos. No método encontre foi criado um for para ver cada elemento e como ele retorna. No método maior doir criado um for e um if para rodar. No método par mais próximo foi criado um while e um for.

--- src/main/App.java
+++ src/main/App.java
@@ -1,14 +1,49 @@
 package main;
+
+import java.util.ArrayList;
+
 public class App {
     public static int encontre(int[] arrayOrdenado, int num){
-        return -1;
+        for(int i=0; i<= arrayOrdenado.length - 1; i++){
+            if(arrayOrdenado[i] == num){
+                return 1;
+            } 
+        } // pecorre elemento por elemento e vai conferir elemento por elemento
+        return 0;
     }
 
     public static int maior(int[] arrayDesordenado){
-        return -1;
+        int maior = arrayDesordenado[0]; // considera a maior valor como o primeiro
+        for(int i = 0; i <= arrayDesordenado.length -1; i++){
+            if(maior < arrayDesordenado[i]){ // se o próximo valor for maior que o anterior ele é substituido 
+                maior = arrayDesordenado[i];
+            }
+        }
+        return maior;
     }
 
-    public static Integer parMaisProximo(Integer[][] pares){
-        return 1;
+    public static Double parMaisProximo(Integer[][] pares){ // mudei para double, pois é valor quebrado
+        ArrayList<Double> menorDistancia = new ArrayList<>();  // uma lista que vai guardando as distâncias
+        while (true) {
+            Integer[] primeiro = pares[0]; // recebe o primeiro par
+            for(int i =1 ; i <= pares.length -1; i++){
+                Integer[] proximo = pares[i]; // recebe o próximo par
+                double tamanho = Math.sqrt(Math.pow((primeiro[0] - proximo[0]),2)  + Math.pow((primeiro[1]- proximo[1]),2)); 
+                // modulo da distância
+                menorDistancia.add(tamanho); // adicionando na lista
+            }
+            pares[0] = null; // tirar o que há foi conferido 
+            if(pares.length < 2){ // enquanto tiver mais de dois pares vai rodar
+                break;
+            }
+        }
+        // mesma lógica do maior lá de cima, mas para o menor valor
+        double menor = menorDistancia.get(0);
+        for(int i = 0; i <= menorDistancia.size() -1; i++){
+            if(menor > menorDistancia.get(i)){
+                menor = menorDistancia.get(i);
+            }
+        }
+        return menor;
     }
 }
